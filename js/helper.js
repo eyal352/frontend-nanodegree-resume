3d0cf75b8cdae9703567f7c3877a6a2bc47c73c5
Eyal <eyal.chistik@gmail.com>
2016-03-20 16:54:00 +0200

    updated all relevant info
    updated all docs, added google map, et.c

--- js/helper.js
+++ js/helper.js
@@ -65,7 +65,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
@@ -85,7 +85,10 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  /* your code goes here!
+    var x = loc.pageX;
+    var y = loc.pageY;
+    logClicks(x,y); */
 });
 
 
@@ -238,11 +241,48 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+map.fitBounds(mapBounds);
+});
+
+// changing the style of the map
+
+    var styles = [
+	{
+		featureType: 'landscape',
+		elementType: 'all',
+		stylers: [
+			{ hue: '#e9e9e9' },
+			{ saturation: -100 },
+			{ lightness: 22 },
+			{ visibility: 'on' }
+		]
+	},{
+		featureType: 'water',
+		elementType: 'geometry',
+		stylers: [
+			{ hue: '#ffff00' },
+			{ saturation: 100 },
+			{ lightness: -34 },
+			{ visibility: 'on' }
+		]
+	}
+];
+var options = {
+	mapTypeControlOptions: {
+		mapTypeIds: [ 'Styled']
+	},
+	center: new google.maps.LatLng(46.38136636533759, 9.83632812500002),
+	zoom: 4,
+	mapTypeId: 'Styled'
+};
+var div = document.getElementById('map');
+var map = new google.maps.Map(div, options);
+var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });
+map.mapTypes.set('Styled', styledMapType);
+
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,51 +1,18 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
-// $("#main").append(["Eyal Chistik"]);
-
-
-var name = "Eyal Chistik";
-var role = "Front End Developer";
-
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$( "#header" ).prepend(formattedRole);
-$( "#header" ).prepend(formattedName);
-
-
-var skills = ["JavaScript", "HTML", "CSS", "Responsive Design", "Python", "Marketing", "Advertising", "Surfing"];
-
+//Variables for bio, work, education, projects
 var bio = {
 	"name": "Eyal Chistik",
     "role": "Front End Developer",
-    "contacts": {"email": "eyal.chistik@gmail.com","github": "eyal352","location": "Tel Aviv"},
+    "contacts": {"email": "eyal.chistik@gmail.com","github": "eyal352", "twitter":"@Eyal352", "location": "Tel Aviv"},
 	"biopic": "https://avatars1.githubusercontent.com/u/16916606?v=3&s=460",
-	"welcomeMessage": "Welcome To Eyal's Bio",
-	 "skills": skills
+	"welcomeMessage": "Welcome To Eyal's Bio Page",
+	 "skills": ["JavaScript", "jQuery", "HTML", "CSS", "Responsive Design", "BootStrap", "Python", "Marketing", "Advertising", "Design", "Surfing"]
 };
 
-var education = {
-	"schools" : [
-		{
-			"name" : "San Francisco State University",
-			"location" : "San Francisco, CA, USA",
-			"degree" : "BA",
-			"major" : ["International Relations"],
-			"dates" : " ",
-			"url" : "http://www.sfsu.edu"
-		},
-		{
-			"name" : "Udacity",
-			"location" : "Online, CyberSpace",
-			"degree" : "Nanodegree Credential",
-			"major" : ["Front End Web Development"],
-			"dates" : " ",
-			"url" : "http://www.udacity.com"
-		}
-	]
+var contactItem = {
+    "email" : "email",
+    "github" : "github",
+    "twitter" : "twitter",
+    "location" : "location"
 };
 
 var work = {
@@ -55,14 +22,16 @@ var work = {
 			"Title" : "Customer Relations Manager",
 			"Location" : "Tel Aviv, Israel",
 			"Dates" : "January 2015 - February 2016",
-			"Description" : "• Provide comprehensive solutions to advertisers to help reach their marketing goals and KPIs through digital advertising campaigns • Build custom sales strategies and perform market research to find potential fields of interests and clients • Negotiate Insertion Order contracts with global clients • Manage multiple pixel and subID implementations for tracking and optimizing campaigns • Build and implement S2S and other tracking links (Appsflyer, Adjust, Tune, etc.) to analyze and optimize digital campaigns •Manage and scale multiple mobile advertising and marketing campaigns across different Geos"
+			"Description" : "Provide comprehensive solutions to advertisers to help reach their marketing goals and KPIs through digital advertising campaigns. Manage multiple pixel and subID implementations for tracking and optimizing campaigns. Build and implement S2S and other tracking links (Appsflyer, Adjust, Tune, etc.) to analyze and optimize digital campaigns •Manage and scale multiple mobile advertising and marketing campaigns across different countries and regions.",
+            "url" : "http://www.adgorithms.com/"
 		},
 		{
 			"Employer" : "Coca-Cola",
 			"Title" : "Market Development Manager",
 			"Location" : "San Francisco, CA",
 			"Dates" : "June 2012 - September 2014",
-			"Description" : "Recognized among the top 10 associates for executing sales initiatives and establishing new business partners within the San Francisco Bay Area. Implemented processes and metrics to drive customer satisfaction, client retention and growth. Designed marketing and sales materials to increase consumer awareness and consumption. Executed and closed sales operations and contractual agreements focused on value added selling activities. Developed and analyzed reports outlining expansion prospects including cost analysis, growth, and short and long term planning."
+			"Description" : "Recognized among the top 10 associates for executing sales initiatives and establishing new business partners within the San Francisco Bay Area. Implemented processes and metrics to drive customer satisfaction, client retention and growth. Designed marketing and sales materials to increase consumer awareness and consumption. Executed and closed sales operations and contractual agreements focused on value added selling activities. Developed and analyzed reports outlining expansion prospects including cost analysis, growth, and short and long term planning.",
+            "url" : "http://www.coca-colacompany.com/"
 		}
 	]
 };
@@ -71,58 +40,88 @@ var projects = {
 	"projects" : [
 		{
 			"Title" : "StudioMunk",
-			"Dates" : " ",
+			"Dates" : "2013",
 			"Description" :"An online music streaming platform integrating social media and allowing users to stream and share their favorite music in an ad-free environment.",
-			"Images" : ["http://www.studiomunk.com/images/toplogo.png"]
+			"Images" : ["http://www.studiomunk.com/images/toplogo.png"],
+            "url" : "http://www.studiomunk.com/"
 		}
 	]
+};
+
+var education = {
+	"schools" : [
+		{
+			"name" : "San Francisco State University",
+			"location" : "San Francisco, CA",
+			"degree" : "Bachelor of Arts",
+			"major" : "International Relations",
+			"dates" : "2007 - 2011",
+			"url" : "http://www.sfsu.edu"
+		},
+		{
+			"name" : "Udacity",
+			"location" : "Mountain View, CA",
+			"degree" : "Nanodegree Credential",
+			"major" : "Front End Web Development",
+			"dates" : "January 2016 - Ongoing",
+			"url" : "http://www.udacity.com"
+		}
+	]
+};
+
+// functions for bio, work, projects, education, contact
+
+bio.display = function() {
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var bioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    var welcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").prepend(formattedRole).prepend(formattedName);
+    $("#header").append(bioPic);
+    $("#header").append(welcomeMsg);
+    $("#header").append(HTMLskillsStart);
+
+    for (contact in bio.contacts) {
+        formattedContact = HTMLcontactGeneric.replace("%contact%", contactItem[contact]).replace("%data%", bio.contacts[contact]);
+        $("#footerContacts").append(formattedContact);
+    }
+
+    for (skill in bio.skills) {
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+    $("#skills").append(formattedSkills);
+
+    }
+
 };
 
 projects.display = function() {
     for (project in projects.projects) {
     $("#projects").append(HTMLprojectStart);
 
-    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].Title);
+    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].Title).replace("#", projects.projects[project].url);
     $(".project-entry:last").append(formattedTitle);
+
+    var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].Dates);
+    $(".project-entry:last").append(formattedDates);
 
     var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].Description);
     $(".project-entry:last").append(formattedDescription);
+
+  //  var formattedImage = HTMLprojectImage.replace("%data%", project.projects[project].Images);
+    //$(".project-entry:last").append(formattedImage);
+    /*    for (image in projects.projects[project].Images) {
+			var formattedImage = HTMLprojectImage.replace("%data%",projects.project[item].images[image]);
+			$(".project-entry:last").append(formattedImage);
+        }; */
     }
 };
 
-projects.display();
-// why am I calling the display function again?
-
-//$("#header").append(education.schools[0].name);
-
-if (bio.skills.length > 0) {
-    $("#header").append(HTMLskillsStart);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[5]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[6]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[7]);
-
-}
-
-
-
-function displayWork() {
+work.display = function () {
 
     for (job in work.jobs) {
     $("#workExperience").append(HTMLworkStart);
 
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].Employer);
+    var formattedEmployer = HTMLworkEmployer.replace("#", work.jobs[job].url).replace("%data%", work.jobs[job].Employer);
 
     var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].Title);
     // concat employer and title
@@ -135,13 +134,34 @@ function displayWork() {
     var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].Dates);
     $(".work-entry:last").append(formattedDates);
 
-    var formattedDescription = HTMLworkDescription.replace("%data", work.jobs[job].Description);
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].Description);
     $(".work-entry:last").append(formattedDescription);
     }
+};
+
+
+education.display = function () {
+
+    for (school in education.schools){
+        $("#education").append(HTMLschoolStart);
+
+        var formattedName = HTMLschoolName.replace("%data%",education.schools[school].name).replace("#", education.schools[school].url);
+
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+
+        var formattedDates = HTMLschoolDates.replace("%data%",education.schools[school].dates);
+		var formattedLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
+		var formattedMajor = HTMLschoolMajor.replace("%data%",education.schools[school].major);
+
+        $(".education-entry:last").append(formattedName + formattedDegree, formattedDates, formattedLocation, formattedMajor);
+    }
+
 }
 
-displayWork();
-
+bio.display();
+work.display();
+education.display();
+projects.display();
 
 $(document).click(function(loc) {
   // your code goes here
@@ -150,12 +170,16 @@ $(document).click(function(loc) {
     logClicks(x, y);
 });
 
-function inName(name){
-	console.log(name);
-	var newName = name;
-	newName = newName[0].toUpperCase() + newName.slice(1,newName.indexOf(" ") + 1).toLowerCase() + newName.slice(newName.indexOf(" ") + 1).toUpperCase(); 
+/*
+function inName(name) {
+    name = name.trim().split(" "); // trim() will trim any white space before and after the string
+    console.log(name);
+    name[1] = name[1].toUpperCase();
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
 
-	return newName;
-};
+    return name[0] +" "+ name[1];
+}; */
 
-$("#main").append(internationalizeButton);
+//$("#main").append(internationalizeButton);
+
+$("#mapDiv").append(googleMap);
